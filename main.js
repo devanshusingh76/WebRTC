cd0e70ad03a26c7b4f6a778322217e9f94aa07a3
devanshusingh76 <rajputdevanshu9084@gmail.com>
2023-06-09 13:59:39 +0530

    firstcommit

---
+++ main.js
@@ -0,0 +1,44 @@
+let divSelectRoom = document.getElementById("selectRoom")
+let divConsultingRoom = document.getElementById("consultingRoom")
+let inputRoomNumber = document.getElementById("roomNumber")
+let btnGoRoom = document.getElementById("goRoom")
+let localVideo = document.getElementById("localVideo")
+let remoteVideo = document.getElementById("remoteVideo")
+
+let roomNumber, localStream, remoteStream, rtcPeerConnection, isCaller
+
+const iceServer = {
+    'iceServer': [
+        { 'urls': 'stun:stun.service.mozilla.com' },
+        { 'urls': 'stun:stun.l.google.com:19302' }
+    ]
+}
+const streamConstraints = {
+    audio: true,
+    video: true
+}
+const socket = io()
+
+btnGoRoom.onclick = () => {
+    if (inputRoomNumber.value === '') {
+        alert("Please type a room name ")
+    }
+    else {
+        roomNumber = inputRoomNumber.value
+        socket.emit('create or join', roomNumber)
+        divSelectRoom.style = "display: none"
+        divConsultingRoom.style = "display: block"
+    }
+}
+
+socket.on('created', room => {
+    navigator.mediaDevices.getUserMedia(streamConstraints)
+        .then(stream => {
+            localStream = stream
+            localVideo.srcObject = stream
+            isCaller = true
+        })
+        .catch(err => {
+            console.log('An error occured', err)
+        })
+})
